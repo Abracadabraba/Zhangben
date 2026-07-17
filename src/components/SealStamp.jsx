b58f30d831e822a2857c6705b792c2418a7b6cf7
Abracadabraba <108131195+Abracadabraba@users.noreply.github.com>
2026-07-17 14:53:31 +0800

    Delete src directory

--- src/components/SealStamp.jsx
+++
@@ -1,7 +0,0 @@
-export default function SealStamp({ char = '来', toast = false }) {
-  return (
-    <span className={`seal${toast ? ' seal--toast' : ''}`} aria-hidden="true">
-      {char}
-    </span>
-  )
-}
